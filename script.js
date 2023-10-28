97597d178df4502abeba335a9959ae90bf00f4b7
rohitupskills <130683027+rohitupskills@users.noreply.github.com>
2023-10-28 23:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
+	if(num === 0){
+        return "0"
+    }
+    let res = "";
+    while(num > 0){
+        let bin = num % 2;
+        res = bin + res;
+        num = Math.floor(num / 2);
+    }
+    return res;
   
 }
 
